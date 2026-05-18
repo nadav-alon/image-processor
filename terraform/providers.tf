7ed17d803fdd8549229b9b195f92939407030a28
Nadav Alon <alonadav2000@gmail.com>
2026-05-18 20:45:17 +0300

    make localstack parametarized

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,22 +1,25 @@
 provider "aws" {
   region                      = "il-central-1"
-  access_key                  = "test"
-  secret_key                  = "test"
-  skip_credentials_validation = true
-  skip_metadata_api_check     = true
-  skip_requesting_account_id  = true
-  s3_use_path_style           = true
+  access_key                  = var.is_localstack ? "test" : null
+  secret_key                  = var.is_localstack ? "test" : null
+  skip_credentials_validation = var.is_localstack
+  skip_metadata_api_check     = var.is_localstack
+  skip_requesting_account_id  = var.is_localstack
+  s3_use_path_style           = var.is_localstack
 
-  endpoints {
-    dynamodb     = "http://localhost:4566"
-    s3           = "http://localhost:4566"
-    sqs          = "http://localhost:4566"
-    iam          = "http://localhost:4566"
-    lambda       = "http://localhost:4566"
-    sts          = "http://localhost:4566"
-    apigateway   = "http://localhost:4566"
-    apigatewayv2 = "http://localhost:4566"
-    sns          = "http://localhost:4566"
-    cloudwatch   = "http://localhost:4566"
+  dynamic "endpoints" {
+    for_each = var.is_localstack ? [1] : []
+    content {
+      dynamodb     = "http://localhost:4566"
+      s3           = "http://localhost:4566"
+      sqs          = "http://localhost:4566"
+      iam          = "http://localhost:4566"
+      lambda       = "http://localhost:4566"
+      sts          = "http://localhost:4566"
+      apigateway   = "http://localhost:4566"
+      apigatewayv2 = "http://localhost:4566"
+      sns          = "http://localhost:4566"
+      cloudwatch   = "http://localhost:4566"
+    }
   }
 }
