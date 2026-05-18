cc1233c63be97c325bc742f2f80ccca4af65d7f3
Nadav Alon <alonadav2000@gmail.com>
2026-05-18 20:50:39 +0300

    s3 hardening

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -33,3 +33,51 @@ resource "aws_s3_bucket_cors_configuration" "thumbnails_cors" {
     max_age_seconds = 3000
   }
 }
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "images_sse" {
+  bucket = aws_s3_bucket.images_bucket.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "images_public_block" {
+  bucket                  = aws_s3_bucket.images_bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_versioning" "images_versioning" {
+  bucket = aws_s3_bucket.images_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "thumbnails_sse" {
+  bucket = aws_s3_bucket.thumbnail_bucket.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "thumbnails_public_block" {
+  bucket                  = aws_s3_bucket.thumbnail_bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_versioning" "thumbnails_versioning" {
+  bucket = aws_s3_bucket.thumbnail_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
